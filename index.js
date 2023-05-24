4fa4f7d3abc6e14e7bcda969081f1f250b70d5be
eross <eross@users.noreply.github.com>
2023-05-24 13:27:31 -0700

    js sandbox hacks

--- index.js
+++ index.js
@@ -4,3 +4,18 @@ import './style.css';
 // Write Javascript code!
 const appDiv = document.getElementById('app');
 appDiv.innerHTML = `<h1>JS Starter</h1>`;
+let x;
+x = 43;
+console.log(x);
+const tst = (function () {
+  function t1(data) {
+    return '56';
+  }
+  return { t1 };
+})();
+
+console.log(tst.t1(44));
+
+const sum = (x, y) => x + y;
+
+sum(4, 12);
